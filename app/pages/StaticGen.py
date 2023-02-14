3c2a803d085f760919a1ea3f564664f8c3fb1020
Aymen311 <berrich311@gmail.com>
2023-02-14 22:23:30 +0100

    quick fix

--- app/pages/StaticGen.py
+++ app/pages/StaticGen.py
@@ -8,7 +8,7 @@ import json
 
 import os
 
-def file_selector(folder_path='./pages'):
+def file_selector(folder_path='.'):
     filenames = os.listdir(folder_path)
     selected_filename = st.selectbox('Select a file', filenames)
     return os.path.join(folder_path, selected_filename)
